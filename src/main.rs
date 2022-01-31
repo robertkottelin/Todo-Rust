96fd77cbd0b3e8f7ced270902af814d8bc5092cf
Robert Kottelin <74188272+robertkottelin@users.noreply.github.com>
2022-01-31 20:21:49 +0100

    First

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::str::FromStr;
+
+fn main() {
+    let action = env::args().nth(1).expect("Please specify an action");
+    let item = env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?}, {:?}", action, item);
+
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
+    if action == "add" {
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' is not present in the list", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("todo saved"),
+                Err(why) => println!("An error occurred: {}", why),
+            },
+        }
+    }
+}
+
+impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+    /*
+    fn new() -> Result<Todo, std::io::Error> {
+        // open the db file
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        // read its content into a new string
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+
+        // allocate an empty HashMap
+        let mut map = HashMap::new();
+
+        // loop over each lines of the file
+        for entries in content.lines() {
+            // split and bind values
+            let mut values = entries.split('\t');
+            let key = values.next().expect("No Key");
+            let val = values.next().expect("No Value");
+            // insert them into HashMap
+            map.insert(String::from(key), bool::from_str(val).unwrap());
+        }
+        // Return Ok
+        Ok(Todo { map })
+    }
+    */
+
+    fn insert(&mut self, key: String) {
+        // insert a new item into our map.
+        // we pass true as value
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
+    }
+}
+
+struct Todo {
+    // use rust built in HashMap to store key - val pairs
+    map: HashMap<String, bool>,
+}
