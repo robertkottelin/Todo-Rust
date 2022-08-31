353c986d51ea94d5fb353eb5ffdf4dd9c553d85a
Robert Kottelin <robert.kottelin@gmail.com>
2022-08-31 21:06:13 +0200

    final sqlite cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,14 @@ fn main() {
     let item = std::env::args().nth(2).expect("Please specify an item");
 
     println!("{:?}, {:?}", action, item);
-
-    let input1: String = String::from(&item.to_string());
-
     let mut todo = Todo::new().expect("Initialisation of todo failed");
 
     if action == "add" {
-        // TODO add from Hashmap
+        // TODO add HashMap instead of inputs
+        // Call a function in main, instead of this
         let conn = Connection::open("todo.db").unwrap();
 
-        let input2 = String::from("True");
-
         let input3 = 0;
-
-
         conn.execute(
             "create table if not exists todo_table (
                 id integer primary key,
@@ -34,7 +28,7 @@ fn main() {
 
         conn.execute(
             "INSERT INTO todo_table (todo, completed) values (?1, ?2)",
-            ([&input1, &input3.to_string()]),
+            ([&item, &input3.to_string()]),
         ).unwrap();
         todo.insert(item);
         match todo.save() {
@@ -42,6 +36,8 @@ fn main() {
             Err(why) => println!("An error occurred: {}", why),
         }
     } else if action == "complete" {
+        // TODO add HashMap instead of inputs
+        // Call a function in main, instead of this
         let conn = Connection::open("todo.db").unwrap();
         conn.execute(
             "create table if not exists todo_table (
@@ -54,7 +50,7 @@ fn main() {
 
         conn.execute(
             "UPDATE todo_table SET completed = 1 WHERE todo = (?1)",
-            ([&input1]),
+            ([&item]),
         ).unwrap();
         match todo.complete(&item) {
             None => println!("'{}' is not present in the list", item),
@@ -67,37 +63,13 @@ fn main() {
     
 }
 
-// fn sqlite() -> Result<()> {
-//     let conn = Connection::open("todo.db")?;
-
-//     let input = String::from("Brew Coffee");
-//     let input2 = String::from("True");
-//     // let input3 = String::from("True");
-
-//     conn.execute(
-//         "create table if not exists test_table (
-//             id integer primary key,
-//             todo text not null unique,
-//             completed text not null unique
-//         )",
-//         ([]),
-//     )?;
-
-//     conn.execute(
-//         "INSERT INTO test_table (todo, completed) values (?1, ?2)",
-//         ([&item, &item]),
-//     )?;
-
-
-//     Ok(())
-// }
-
 #[derive(Debug)]
 struct Todo {
     // use rust built in HashMap to store key - val pairs
     map: HashMap<String, bool>,
 }
 
+// TODO implement iterator trait for TODO and iterate over the HashMap when interacting with sqlite
 // impl Iterator for Todo {
 //     type Item = String;
 // }
@@ -122,6 +94,10 @@ impl Todo {
     }
 
     // db.txt
+    // This bit of code demonstrates two examples of how you can 
+    //write to a txt file instead of to a json or sqlite
+
+    // 1
     // fn new() -> Result<Todo, std::io::Error> {
     //     let mut f = std::fs::OpenOptions::new()
     //         .write(true)
@@ -139,6 +115,7 @@ impl Todo {
     //     Ok(Todo { map })
     // }
 
+    // 2
     // fn new() -> Result<Todo, std::io::Error> {
     //     // open the db file
     //     let mut f = std::fs::OpenOptions::new()
@@ -184,7 +161,7 @@ impl Todo {
         Ok(())
     }
 
-    // db.txt
+    // save to db.txt instead of JSON
     // fn save(self) -> Result<(), std::io::Error> {
     //     let mut content = String::new();
     //     for (k, v) in self.map {
