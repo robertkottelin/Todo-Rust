5dfeda5d7c086f586c112d0011dc43fa27a3174b
Robert Kottelin <robert.kottelin@gmail.com>
2022-08-31 11:35:59 +0200

    update json

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 use std::collections::HashMap;
-use std::env;
-use std::fs::File;
-use std::io;
-use std::io::Read;
-use std::str::FromStr;
 
 fn main() {
-    let action = env::args().nth(1).expect("Please specify an action");
-    let item = env::args().nth(2).expect("Please specify an item");
-
-    println!("{:?}, {:?}", action, item);
+    let action = std::env::args().nth(1).expect("Please provide an action");
+    let item = std::env::args().nth(2).expect("Please provide an item");
 
     let mut todo = Todo::new().expect("Initialisation of db failed");
 
     if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
     } else if action == "complete" {
         match todo.complete(&item) {
             None => println!("'{}' is not present in the list", item),
@@ -23,68 +21,44 @@ fn main() {
             },
         }
     }
-    //println!("{:?}", &todo)
+}
+
+struct Todo {
+    // use rust built in HashMap to store key - val pairs
+    map: HashMap<String, bool>,
 }
 
 impl Todo {
     fn new() -> Result<Todo, std::io::Error> {
-        let mut f = std::fs::OpenOptions::new()
+        let f = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
-            .open("db.txt")?;
-        let mut content = String::new();
-        f.read_to_string(&mut content)?;
-        let map: HashMap<String, bool> = content
-            .lines()
-            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
-            .map(|v| (v[0], v[1]))
-            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
-            .collect();
-        Ok(Todo { map })
-    }
-    /*
-    fn new() -> Result<Todo, std::io::Error> {
-        // open the db file
-        let mut f = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .read(true)
-            .open("db.txt")?;
-        // read its content into a new string
-        let mut content = String::new();
-        f.read_to_string(&mut content)?;
+            .open("db.json")?;
 
-        // allocate an empty HashMap
-        let mut map = HashMap::new();
-
-        // loop over each lines of the file
-        for entries in content.lines() {
-            // split and bind values
-            let mut values = entries.split('\t');
-            let key = values.next().expect("No Key");
-            let val = values.next().expect("No Value");
-            // insert them into HashMap
-            map.insert(String::from(key), bool::from_str(val).unwrap());
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }),
+            Err(e) if e.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            Err(e) => panic!("An error occurred: {}", e),
         }
-        // Return Ok
-        Ok(Todo { map })
     }
-    */
-
     fn insert(&mut self, key: String) {
         // insert a new item into our map.
-        // we pass true as value
+        // active state is set to true by default.
         self.map.insert(key, true);
     }
 
-    fn save(&mut self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k, v) in &mut self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record)
-        }
-        std::fs::write("db.txt", content)
+    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open("db.json")?;
+        serde_json::to_writer_pretty(f, &self.map)?;
+
+        Ok(())
     }
 
     fn complete(&mut self, key: &String) -> Option<()> {
@@ -93,10 +67,4 @@ impl Todo {
             None => None,
         }
     }
-}
-
-#[derive(Debug)]
-struct Todo {
-    // use rust built in HashMap to store key - val pairs
-    map: HashMap<String, bool>,
 }
